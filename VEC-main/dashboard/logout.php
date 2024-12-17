5088022aa015ac4d0d951112d8a3362ddca6559a
vec <vec.pbl@gmail.com>
2024-12-17 12:25:33 +0700

    Remove Folder

--- VEC-main/dashboard/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-    /*setcookie('id', '', -1, '/');
-    setcookie('key', '', -1, '/');*/
-    header('location: ../')
-?>
--- VEC-main/service/autoload.php
+++
@@ -1,3 +0,0 @@
-<?php
-include __DIR__.'/database.php';
-include __DIR__.'/function.php';
